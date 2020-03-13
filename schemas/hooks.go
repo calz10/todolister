b14a58ae7e46d67d423653152d7cac811d08b9b6
caldz10 <calderonisrael773@gmail.com>
2020-03-13 23:23:32 +0800

    chore(hooks): Added hooks function for user creation

---
+++ schemas/hooks.go
@@ -0,0 +1,22 @@
+package schemas
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+)
+
+//  ------------------------------------
+//  afterCreate hooks
+// --------------------------------------
+
+// User Hooks
+
+// AfterCreate user hooks
+func (u *User) AfterCreate(scope *gorm.Scope) (err error) {
+	if u.ID > 0 {
+		scope.DB().Create(Profile{UserId: fmt.Sprint(u.ID)})
+	}
+
+	return
+}
